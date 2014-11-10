955006e087499d0624f877dfd3f05176c9839131
Kurtis Williams <kurtisdwilliams@gmail.com>
2014-11-10 03:09:38 +0000

    fixed home page authenicate sign up to continue

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, expect: [:index, :show]
+
 
   def index
     @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 50)
