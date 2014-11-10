7f87fb08a10edee9b26ce5996c1a15d75cf9ff34
Kurtis Williams <kurtisdwilliams@gmail.com>
2014-11-10 04:09:43 +0000

    Live Jumbo App

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# One Month Rails
+# Jumbo App
 
-Laura smells and looks like a granny, she is currently licking my arm and annoying me, please make her stop. OH NO SHE JUST FARTED LOL.
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pins
 
-  devise_for :users 
+  devise_for :users
   root "pins#index"
   get "about" => "pages#about"
   get "contactus" => "pages#contactus" # creates about_path
